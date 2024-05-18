85a233abb23dae88bdbd27e2a292eade6d65e438
EspirituMissyAnneJhelzshir <146043344+EspirituMissyAnneJhelzshir@users.noreply.github.com>
2024-05-18 11:20:54 +0800

    Update final_exam_model_deployment_in_the_cloud.py

--- final_exam_model_deployment_in_the_cloud.py
+++ final_exam_model_deployment_in_the_cloud.py
@@ -48,11 +48,10 @@ from keras.utils import load_img
 from keras.utils import img_to_array
 from keras.utils import to_categorical
 
+pip install streamlit
 import os
-
 # Run Streamlit using os.system()
 os.system('streamlit run /usr/local/lib/python3.10/dist-packages/colab_kernel_launcher.py')
-
 import streamlit as st
 import numpy as np
 import tensorflow as tf
